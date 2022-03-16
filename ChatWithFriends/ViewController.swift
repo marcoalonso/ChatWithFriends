f7946a316d35351c96e17aee13b3be91300728ed
marcoalonso <marcoalonsorota@hotmail.com>
2022-03-15 20:32:58 -0600

    Empezando

---
+++ ChatWithFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatWithFriends
+//
+//  Created by marco rodriguez on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
