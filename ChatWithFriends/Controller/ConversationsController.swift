ab9f826518cb98be620f405c28ea25f35a9406ba
marcoalonso <marcoalonsorota@hotmail.com>
2022-03-18 12:28:03 -0600

    SetupMessagesTableView

--- ChatWithFriends/Controller/ConversationsController.swift
+++ ChatWithFriends/Controller/ConversationsController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsController: UIViewController {
     // MARK: - Properties
+    
+    private let tableView = UITableView()
     
     // MARK: - Lifecycle
     
@@ -26,8 +30,8 @@ class ConversationsController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.title = "Messages"
+        configureNavigationBar()
+        configureTableView()
         
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
@@ -35,5 +39,55 @@ class ConversationsController: UIViewController {
         
     }
     
+    func configureTableView(){
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+    }
     
+    func configureNavigationBar() {
+        let appearence = UINavigationBarAppearance()
+        appearence.configureWithOpaqueBackground()
+        appearence.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearence.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearence
+        navigationController?.navigationBar.compactAppearance = appearence
+        navigationController?.navigationBar.scrollEdgeAppearance = appearence
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        
+        
+    }
+    
+    
+}
+
+
+extension ConversationsController:  UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Marco"
+        return  cell
+    }
+    
+    
+}
+
+extension ConversationsController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 }
